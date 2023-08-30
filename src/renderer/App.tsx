6398c2bb29e78db00122b97749c73de8699a1285
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-08-30 01:03:16 +0100

    v1.0.2
    added forgot password page

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,15 +4,21 @@ import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 
 import './App.css';
 import Home from '@Components/home/Home';
+import ForgotPassword from '@Components/forgot-password/ForgotPassword';
+import NavBar from '@Components/nav-bar/NavBar';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <NavBar />
+      <div className="content-pane">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/renderer/Components/nav-bar/NavBar.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './NavBar.scss';
+
+function NavBar() {
+  return <div className="nav-bar-container"></div>;
+}
+
+export default NavBar;
